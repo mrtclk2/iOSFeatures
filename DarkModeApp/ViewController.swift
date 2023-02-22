5b1aba50d62a3070c4368d8556b6c43096612f9a
Murat Çelik <muratclik2@gmail.com>
2023-02-22 21:22:02 +0300

    first commit

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+        override func viewWillAppear(_ animated: Bool) {
+                let userInterfaceStyle = traitCollection.userInterfaceStyle
+                      
+                      if userInterfaceStyle == .dark {
+                          changeButton.tintColor = UIColor.white
+                      } else {
+                          changeButton.tintColor = UIColor.blue
+                      }
+                      
+            }
+            
+            override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+                let userInterfaceStyle = traitCollection.userInterfaceStyle
+                            
+                            if userInterfaceStyle == .dark {
+                                changeButton.tintColor = UIColor.white
+                            } else {
+                                changeButton.tintColor = UIColor.blue
+                            }
+            }
+            
+            
 
 
-}
-
+        }
