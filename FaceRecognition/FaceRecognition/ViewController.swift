435d446dd5d33a54461142bba3c79d43a82a1799
Murat Çelik <121906685+mrtclk2@users.noreply.github.com>
2023-02-23 00:14:54 +0300

    FaceRecognition

---
+++ FaceRecognition/FaceRecognition/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by Murat Çelik on 22.02.2023.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+    @IBAction func signInClicked(_ sender: Any) {
+    
+        let authContext = LAContext()
+                
+                var error: NSError?
+                
+                if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                    
+                    authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                        if success == true {
+                            //successful auth
+                            DispatchQueue.main.async {
+                                self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                            }
+                        } else {
+                            DispatchQueue.main.async {
+                                self.myLabel.text = "Error!"
+                            }
+                        }
+                    }
+            }
+    }
+}
